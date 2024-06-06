74b1e630d663ba234a090b76c3365c174dd28ef0
Kevin Schaper <kevinschaper@gmail.com>
2024-06-06 14:37:56 -0700

    add source map, fix aggregator knowledge_source, update cruft template for doc

--- docs/index.md
+++ docs/index.md
@@ -3,3 +3,6 @@
 {{ get_nodes_report() }}
 
 {{ get_edges_report() }}
+
+{ % include 'docs/nodes_report.md' %}
+{ % include 'docs/edges_report.md' %}
--- src/alliance_genotype/transform.py
+++ src/alliance_genotype/transform.py
@@ -8,6 +8,17 @@ from koza.cli_utils import get_koza_app
 
 koza_app = get_koza_app("alliance_genotype")
 
+source_map = {
+    "FB": "infores:flybase",
+    "MGI": "infores:mgi",
+    "RGD": "infores:rgd",
+    "HGNC": "infores:rgd",  # Alliance contains RGD curation of human genes
+    "SGD": "infores:sgd",
+    "WB": "infores:wormbase",
+    "Xenbase": "infores:xenbase",
+    "ZFIN": "infores:zfin",
+}
+
 while (row := koza_app.get_row()) is not None:
     # Code to transform each row of data
     # For more information, see https://koza.monarchinitiative.org/Ingests/transform
@@ -28,11 +39,9 @@ while (row := koza_app.get_row()) is not None:
             subject=genotype.id,
             predicate="biolink:has_variant_participant",
             object=allele["alleleID"],
-            # TODO: capture zygosity
             qualifier=allele["zygosity"] if "zygosity" in allele else None,
-            # TODO: replace with MOD infores ID, based on prefix?
-            primary_knowledge_source="infores:alliance",
-            aggregator_knowledge_source=["infores:monarchinitiative", "infores:alliance"],
+            primary_knowledge_source=source_map[row["objectId"].split(':')[0]],
+            aggregator_knowledge_source=["infores:monarchinitiative", "infores:agrkb"],
             knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
             agent_type=AgentTypeEnum.manual_agent,
         )
